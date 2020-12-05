1b6cc8a00005ea58af43468b7586dcb372bd6bb3
Kenny Loveall <kenny@shipwell.com>
2020-12-04 23:37:50 -0600

    Cleanup day 4

--- day04.py
+++ day04.py
@@ -5,11 +5,11 @@ from typing import List
 
 def process_line(line):
     passport_parts = line.strip().split(" ")
-    if passport_parts == ['']:
+    if passport_parts == ['']:  # Can't just do not passport_parts; `not ['']` == False, not True like we need
         return {}
     ret_val = {}
     for part in passport_parts:
-        key,value = part.split(":")
+        key, value = part.split(":")
         ret_val[key] = value
     return ret_val
 
@@ -30,83 +30,109 @@ def read_input(file_name: str):
 
 def solvep1(input):
     valid = 0
-    print(len(input))
     for passport in input:
         if 'byr' not in passport:
             continue
-        if not re.match('[0-9]{4}', passport['byr']):
-            continue
-        try:
-            birth_year = int(passport['byr'])
-        except ValueError:
-            continue
-        if not (1920 <= birth_year <= 2002):
-            continue
-
         if 'iyr' not in passport:
             continue
-        if not re.match('[0-9]{4}', passport['iyr']):
-            continue
-        try:
-            issue_year = int(passport['iyr'])
-        except ValueError:
-            continue
-        if not (2010 <= issue_year <= 2020):
-            continue
-
         if 'eyr' not in passport:
             continue
-        if not re.match('[0-9]{4}', passport['eyr']):
-            continue
-        try:
-            expiration_year = int(passport['eyr'])
-        except ValueError:
-            continue
-        if not (2020 <= expiration_year <= 2030):
-            continue
-
         if 'hgt' not in passport:
             continue
-        if not re.match('1?[0-9]{2}[cm|in]', passport['hgt']):
-            continue
-        try:
-            height_val = int(passport['hgt'][:-2])
-            height_units = passport['hgt'][-2:]
-        except ValueError:
-            continue
-        if height_units == 'cm':
-            if not (150 <= height_val <= 193):
-                continue
-        elif height_units == "in":
-            if not (59 <= height_val <= 76):
-                continue
-        else:
-            continue
-
         if 'hcl' not in passport:
             continue
-        m = re.match('^#[0-9a-f]{6}$', passport['hcl'])
-        if not m:
-            continue
-
         if 'ecl' not in passport:
             continue
-        if passport['ecl'] not in ('amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'):
-            continue
-
         if 'pid' not in passport:
             continue
-
-        m = re.match('^[0-9]{9}$', passport['pid'])
-        if not m:
-            continue
-
         valid += 1
     return valid
 
 
+def is_birth_year_valid(birth_year_str: str):
+    if not re.match('[0-9]{4}', birth_year_str):
+        return False
+    try:
+        birth_year = int(birth_year_str)
+    except ValueError:
+        return False
+    if not (1920 <= birth_year <= 2002):
+        return False
+    return True
+
+
+def is_issue_year_valid(issue_year_str: str):
+    if not re.match('[0-9]{4}', issue_year_str):
+        return False
+    try:
+        issue_year = int(issue_year_str)
+    except ValueError:
+        return False
+    if not (2010 <= issue_year <= 2020):
+        return False
+    return True
+
+
+def is_expire_year_valid(expire_year_str: str):
+    if not re.match('[0-9]{4}', expire_year_str):
+        return False
+    try:
+        expiration_year = int(expire_year_str)
+    except ValueError:
+        return False
+    if not (2020 <= expiration_year <= 2030):
+        return False
+    return True
+
+
+def is_height_valid(height_str: str):
+    regex_result = re.match('^(1?[0-9]{2})((cm)|(in))$', height_str)
+    if not regex_result:
+        return False
+    height_val = int(regex_result.group(1))
+    height_units = regex_result.group(2)
+    if height_units == 'cm':
+        if not (150 <= height_val <= 193):
+            return False
+    elif height_units == "in":
+        if not (59 <= height_val <= 76):
+            return False
+    else:
+        raise RuntimeError
+    return True
+
+
+def is_hair_color_valid(hair_color_str: str):
+    return bool(re.match('^#[0-9a-f]{6}$', hair_color_str))
+
+
+def is_eye_color_valid(eye_color_str: str):
+    return bool(eye_color_str in ('amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'))
+
+
+def is_passport_id_valid(passport_id_str: str):
+    return bool(re.match('^[0-9]{9}$', passport_id_str))
+
+
 def solvep2(input):
-    return input
+    valid = 0
+    for passport in input:
+        if 'byr' not in passport or not is_birth_year_valid(passport['byr']):
+            continue
+        if 'iyr' not in passport or not is_issue_year_valid(passport['iyr']):
+            continue
+        if 'eyr' not in passport or not is_expire_year_valid(passport['eyr']):
+            continue
+        if 'hgt' not in passport or not is_height_valid(passport['hgt']):
+            continue
+        if 'hcl' not in passport or not is_hair_color_valid(passport['hcl']):
+            continue
+        if 'ecl' not in passport or not is_eye_color_valid(passport['ecl']):
+            continue
+        if 'pid' not in passport or not is_passport_id_valid(passport['pid']):
+            continue
+        valid += 1
+    return valid
 
 
 if __name__ == '__main__':
