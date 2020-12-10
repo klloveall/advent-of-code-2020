86d3b4b41ba4b964da494371e185564535aab685
Kenny Loveall <kenny@shipwell.com>
2020-12-09 23:27:39 -0600

    Days 9 & 10

--- day10.py
+++ day10.py
@@ -31,46 +31,38 @@ def read_input(file_name: str,
 class Exc(Exception):
     pass
 
-def num_is_sum_of_two_numbers_in_list(target_number, list_of_nums):
-    list_of_nums.sort()
-    low_ptr = 0
-    high_ptr = len(list_of_nums) - 1
-    while low_ptr != high_ptr and list_of_nums[low_ptr] + list_of_nums[high_ptr] != target_number:
-        if list_of_nums[low_ptr] + list_of_nums[high_ptr] > target_number:
-            high_ptr -=1
-        else:
-            low_ptr += 1
-    if low_ptr == high_ptr:
-        return None, None
-    else:
-        return list_of_nums[low_ptr], list_of_nums[high_ptr]
-
 def solvep1(input):
-    for i, num in enumerate(input[25:]):
-        start_i = i
-        end_i = i + 25
-        num_1, num_2 = num_is_sum_of_two_numbers_in_list(num, input[start_i:end_i])
-        if num_1 is None:
-            return num
+    diff_1 = 0
+    diff_3 = 1
+    input.sort()
+    last = 0
+    for i in input:
+        if i - last == 1:
+            diff_1 +=1
+        elif i - last == 3:
+            diff_3 += 1
+        else:
+            raise RuntimeError()
+        last=i
+    return diff_1 * diff_3
+
 
 def solvep2(input):
-    target_num = solvep1(input)
-    low_ptr = 0
-    high_ptr = 1
-    range_sum = sum(input[low_ptr:high_ptr])
-    while range_sum != target_num:
-        if range_sum > target_num or high_ptr == len(input) + 1:
-            low_ptr += 1
-            high_ptr = low_ptr + 1
-        else:
-            high_ptr += 1
-        range_sum = sum(input[low_ptr:high_ptr])
-    return min(input[low_ptr:high_ptr]) + max(input[low_ptr:high_ptr])
+    sums = [0] * (input[-1] +  1)
+    sums [0] = 1
+    for num in input:
+        sums[num] += sums[num-1]
+        if num >= 2:
+            sums[num] += sums[num-2]
+        if num >= 3:
+            sums[num] += sums[num-3]
+    return sums[-1]
+    return input
 
 
 
 if __name__ == '__main__':
-    processed_input = read_input('day09-input.txt')
+    processed_input = read_input('day10-input.txt')
     output1 = solvep1(processed_input)
     print(output1)
     output2 = solvep2(processed_input)
